3fafaed9e948dd85a91f50add426922713f8b123
Ubuntu <ubuntu@ip-172-31-11-155.ap-northeast-1.compute.internal>
2021-06-11 15:25:23 +0000

    음식점 작업중

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -2,18 +2,17 @@ class MapController < ApplicationController
     def worldmap
         @pins = Pin.all
         @restinfos = Restinfo.all
-        @datepick = params[:datepick]
-        @name = params[:name]
-        @with = params[:with]
-        @menu = params[:menu]
-        @anything = params[:anything]
     end
     
     def ticket
-        @datepick = params[:datepick]
-        @name = params[:name]
-        @with = params[:with]
-        @menu = params[:menu]
-        @anything = params[:anything]
+        @ticket = Ticket.new(ticket_params)
+        
+        
+        redirect_to "/map#modal1"
+    end
+
+private 
+    def ticket_params
+        params.require(:ticket).permit(:datepick, :name, :with, :menu, :anything)
     end
 end
