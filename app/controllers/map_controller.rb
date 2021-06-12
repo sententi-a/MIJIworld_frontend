bd38159f3354aed31d449a8a8ef39ae5de0b53f9
Jiwon Song <sjw_512@naver.com>
2021-06-13 00:36:53 +0900

    모달 변수 전달 시도

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -2,6 +2,7 @@ class MapController < ApplicationController
     def worldmap
         @pins = Pin.all
         @restinfos = Restinfo.all
+        #@restid = params[:restid]
     end
     
     def ticket
@@ -9,6 +10,15 @@ class MapController < ApplicationController
         
         
         redirect_to "/map#modal1"
+    end
+
+    def show_modal
+        @restinfo = Restinfo.Find(params[:restid])
+    end
+
+    def show_modal_test
+        logger.debug "got restid: #{params[:restid]}"
+        
     end
 
 private 
