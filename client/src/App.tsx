203102de8a69ae1e44e2fe8aae16587f30d19a94
HyejiYeom <kristi8041@gmail.com>
2023-04-25 16:40:18 +0900

    Feat : Add react-router-dom

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,17 @@
 import React from "react";
-import logo from "./logo.svg";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import "./styles/font.css";
 import Mainpage from "./components/Mainpage";
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <Mainpage />
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-    </div>
+      <Routes>
+        <Route path="/" element={<Mainpage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
