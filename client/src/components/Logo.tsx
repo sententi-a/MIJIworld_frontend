299bdddf134cfcf6ae55e9e1a444c0c80bd79e84
HyejiYeom <kristi8041@gmail.com>
2023-04-25 20:45:38 +0900

    Feat : Implement map page (component)

---
+++ client/src/components/Logo.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Logo = styled.img`
+  width: 10vw;
+  margin-top: 2vw;
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%);
+`;
+
+export default Logo;
--- client/src/components/Map.tsx
+++ client/src/components/Map.tsx
@@ -1,9 +1,29 @@
 import React from "react";
+import styled from "styled-components";
+import BackImage from "./BackImage";
+import Bg from "../assets/images/map/worldmap_background.png";
+import MapBg from "../assets/images/map/worldmap.png";
+import Logo from "./Logo";
+import LogoPath from "../assets/images/logo/logo.png";
 
 export default function Map() {
   return (
     <>
-      <h1>안녕map</h1>
+      <BackImage path={Bg}>
+        <Logo src={LogoPath} />
+        <WorldMap></WorldMap>
+      </BackImage>
     </>
   );
 }
+
+const WorldMap = styled.div`
+  background-image: url(${MapBg});
+  background-size: 85vw 70vh;
+  width: 85vw;
+  height: 70vh;
+  top: 52%;
+  left: 50%;
+  position: absolute;
+  transform: translate(-50%, -50%);
+`;
