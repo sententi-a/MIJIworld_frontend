3cc12018701f0ed51e1ad31eb6b992a2b22aab0a
HyejiYeom <kristi8041@gmail.com>
2023-04-26 14:30:41 +0900

    Feat : Add route path to list page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import "./App.css";
 import "./styles/font.css";
 import Mainpage from "./components/Mainpage";
 import Map from "./components/Map";
+import List from "./components/List";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Mainpage />} />
         <Route path="/map" element={<Map />} />
+        <Route path="/list" element={<List />} />
       </Routes>
     </BrowserRouter>
   );
