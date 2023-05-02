9e904b9ecb89d08069fde3bc4b80caa3d2eec7bf
HyejiYeom <kristi8041@gmail.com>
2023-05-02 17:23:15 +0900

    Style : Customize datepicker style

--- client/src/components/DayPicker.tsx
+++ client/src/components/DayPicker.tsx
@@ -3,9 +3,11 @@ import { useState } from "react";
 import "react-datepicker/dist/react-datepicker.css";
 import styles from "../styles/DayPicker.module.css";
 import { Label } from "../components/CustomInput";
+import ko from "date-fns/locale/ko";
 
 export default function DayPicker() {
   const [selectedDate, setSelectedDate] = useState<Date | null>(new Date());
+
   return (
     <>
       <div
@@ -17,6 +19,8 @@ export default function DayPicker() {
       >
         <Label>날짜</Label>
         <DatePicker
+          dateFormat={"yyyy년 MM월 dd일 (eee)"}
+          locale={ko}
           selected={selectedDate}
           onChange={(date) => setSelectedDate(date)}
           shouldCloseOnSelect
