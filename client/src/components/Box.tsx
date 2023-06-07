aba92a62980d0019b2e53b29ba02768d9d7393a2
HyejiYeom <kristi8041@gmail.com>
2023-06-07 19:47:11 +0900

    Feat : Add shadowIntensity property to Box component

--- client/src/components/Box.tsx
+++ client/src/components/Box.tsx
@@ -3,20 +3,34 @@ import styled from "styled-components";
 
 interface BoxProps extends React.HTMLAttributes<HTMLDivElement> {
   children: React.ReactNode;
+  shadowIntensity?: "strong" | "weak" | "modest";
 }
 
-function Box_({ children, ...props }: BoxProps) {
-  return <BoxDiv {...props}>{children}</BoxDiv>;
+function Box_({ children, shadowIntensity = "modest", ...props }: BoxProps) {
+  return (
+    <BoxDiv className={shadowIntensity} {...props}>
+      {children}
+    </BoxDiv>
+  );
 }
 
 const BoxDiv = styled.div`
-  box-shadow: 5px 5px 10px 0 #cccccc;
   border-radius: 10px;
   justify-content: center;
   align-items: center;
   display: flex;
   flex-direction: column;
   padding: 10px;
+
+  &.weak {
+    box-shadow: rgba(105, 103, 103, 0.118) 0px 0px 10px;
+  }
+  &.modest {
+    box-shadow: 5px 5px 10px 0 #cccccc;
+  }
+  &.strong {
+    box-shadow: 8px 6px 12px 2px #cacaca;
+  }
 `;
 
 const Box = React.memo(Box_);
