9cb42ce869cf8c3d6982d0cbe4840f947fc786b0
HyejiYeom <kristi8041@gmail.com>
2023-06-08 15:56:23 +0900

    Refactor : Lower abstraction level of StarRate component and move it into common component directory

---
+++ client/src/components/common/StarRate.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Image } from "./index";
+
+interface StarRateProps {
+  rate: number;
+}
+
+function StarRate_({ rate }: StarRateProps) {
+  const width = (rate / 5) * 7 + "vw";
+
+  return (
+    <>
+      <div style={{ position: "relative" }}>
+        <div style={{ position: "absolute", overflow: "hidden", width }}>
+          <Image
+            src={require("../../assets/images/icon/star_full.png")}
+            size="medium"
+            style={{ zIndex: 10, width: "7vw" }}
+          />
+        </div>
+        <Image
+          src={require("../../assets/images/icon/star_bin.png")}
+          size="medium"
+          style={{ width: "7vw", top: 0 }}
+        />
+      </div>
+    </>
+  );
+}
+
+const StarRate = React.memo(StarRate_);
+export default StarRate;
--- client/src/components/common/index.ts
+++ client/src/components/common/index.ts
@@ -5,3 +5,4 @@ export { default as Image } from "./Image";
 export { default as Input } from "./Input";
 export { default as Text } from "./Text";
 export { default as Toggle } from "./Toggle";
+export { default as StarRate } from "./StarRate";
