374c353e1d9425baa1c9475f77814f49c36cc61d
HyejiYeom <kristi8041@gmail.com>
2023-06-13 01:25:49 +0900

    [Chore] Install typeorm

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,20 @@
-import express, { Express, Request, Response } from "express";
+import { AppDataSource } from "./data-source"
+import { User } from "./entity/User"
 
-const app: Express = express();
-const port = process.env.PORT || 5500;
+AppDataSource.initialize().then(async () => {
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Ready!");
-});
+    console.log("Inserting a new user into the database...")
+    const user = new User()
+    user.firstName = "Timber"
+    user.lastName = "Saw"
+    user.age = 25
+    await AppDataSource.manager.save(user)
+    console.log("Saved a new user with id: " + user.id)
 
-app.listen(port, () => {
-  console.log(`Server is running at localhost:${port}"`);
-});
+    console.log("Loading users from the database...")
+    const users = await AppDataSource.manager.find(User)
+    console.log("Loaded users: ", users)
+
+    console.log("Here you can setup and run express / fastify / any other framework.")
+
+}).catch(error => console.log(error))
