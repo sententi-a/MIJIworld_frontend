58689e7bed4928a95525f2567eeea0cb315bd7ee
HyejiYeom <kristi8041@gmail.com>
2023-06-15 01:41:37 +0900

    [Fix] Handle axios error making a new axios client

--- client/src/apis/pin.ts
+++ client/src/apis/pin.ts
@@ -1,10 +1,10 @@
-import axios from "axios";
+import api from "./index";
 
-const server = process.env.SERVER || "localhost:5500";
+const server = process.env.SERVER || "http://localhost:5500";
 
 const pinApi = {
   getPins: async () => {
-    const result = await axios.get(`${server}/pin`);
+    const result = await api.get(`/pins`);
     return result.data;
   },
 };
--- client/src/apis/restaurant.ts
+++ client/src/apis/restaurant.ts
@@ -1,11 +1,16 @@
-import axios from "axios";
+import api from "./index";
 
 const server = process.env.SERVER || "localhost:5500";
 
 const restaurantApi = {
   getRestaurants: async () => {
-    const result = await axios.get(`${server}/restaurants`);
+    const result = await api.get(`/restaurants`);
     console.log(result);
+    return result.data;
+  },
+  getDialog: async (name: string) => {
+    console.log(server);
+    const result = await api.get(`/restaurant/dialog/${name}`);
     return result.data;
   },
 };
