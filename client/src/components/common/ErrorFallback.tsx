112578b0d95923ffd0def1396d917a041b367f51
HyejiYeom <kristi8041@gmail.com>
2023-07-02 23:00:50 +0900

    [Style] Change ErrorFallback Box style

--- client/src/components/common/ErrorFallback.tsx
+++ client/src/components/common/ErrorFallback.tsx
@@ -12,13 +12,23 @@ export default function ErrorFallback({
   //   console.log(error);
   return (
     <>
-      <Box style={{}}>
-        <Text text={"⚠️ 데이터를 불러올 수 없습니다."} bold={true} size="t3" />
-        <Text text={"새로고침을 하거나 잠시 후 다시 접속해주세요."} />
+      <Box
+        style={{
+          position: "absolute",
+          backgroundColor: "white",
+          top: "15vw",
+          left: "50%",
+          transform: "translate(-50%)",
+        }}
+        shadowIntensity="weak"
+      >
+        <Text text={"⚠️ 데이터를 불러올 수 없습니다."} bold={true} size="t4" />
+        <Text text={"새로고침을 하거나 잠시 후 다시 접속해주세요."} size="t5" />
         <Button
           onClick={resetErrorBoundary}
           label={"새로고침"}
           style={{ marginTop: "2vh" }}
+          size="small"
         />
       </Box>
     </>
