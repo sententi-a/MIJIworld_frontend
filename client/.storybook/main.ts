3b5bb3d0ae5abe1afc17bcdbae8a784173a1b2fc
HyejiYeom <kristi8041@gmail.com>
2023-07-03 22:22:31 +0900

    [Chore] Change storybook config to use absolute path

--- client/.storybook/main.ts
+++ client/.storybook/main.ts
@@ -1,12 +1,26 @@
 import type { StorybookConfig } from "@storybook/react-webpack5";
-const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  stories: [
+    "../src/stories/*.stories.@(js|jsx|ts|tsx)",
+    "../src/stories/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app",
     "@storybook/addon-interactions",
   ],
+  webpackFinal: async (config) => {
+    config.resolve.plugins.push(
+      new TsconfigPathsPlugin({
+        configFile: path.resolve(__dirname, "../tsconfig.json"),
+      })
+    );
+    return config;
+  },
   framework: {
     name: "@storybook/react-webpack5",
     options: {},
@@ -14,6 +28,4 @@ const config: StorybookConfig = {
   docs: {
     autodocs: "tag",
   },
-  staticDirs: ["../public"],
 };
-export default config;
