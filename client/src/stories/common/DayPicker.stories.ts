4f51a06bbdd8403356478e6a06fc82a21f999743
HyejiYeom <kristi8041@gmail.com>
2023-07-05 22:13:43 +0900

    [Feat] Add docs to SearchBar, DayPicker component stories

--- client/src/stories/common/DayPicker.stories.ts
+++ client/src/stories/common/DayPicker.stories.ts
@@ -4,6 +4,15 @@ import { DayPicker } from "@components/common";
 const meta: Meta<typeof DayPicker> = {
   title: "components/common/DayPicker",
   component: DayPicker,
+  tags: ["autodocs"],
+  parameters: {
+    componentSubtitle:
+      "DayPicker은 날짜를 선택할 수 있는 molecule 컴포넌트입니다.",
+  },
+  argTypes: {
+    date: { description: "DayPicker로 지정한 날짜" },
+    setDate: { description: "date를 변경하는 setState 함수" },
+  },
 };
 
 export default meta;
--- client/src/stories/common/SearchBar.stories.ts
+++ client/src/stories/common/SearchBar.stories.ts
@@ -5,6 +5,17 @@ import { SearchBar } from "@components/common";
 const meta: Meta<typeof SearchBar> = {
   title: "components/common/SearchBar",
   component: SearchBar,
+  tags: ["autodocs"],
+  parameters: {
+    componentSubtitle: "SearchBar은 검색을 위한 molecule 컴포넌트입니다.",
+  },
+  argTypes: {
+    placeholder: {
+      description: "검색창 내 짧은 도움말",
+    },
+    color: { description: "검색창 테두리 및 검색 아이콘 색상" },
+    handleOnChange: { description: "검색어 입력시 호출되는 콜백 함수" },
+  },
 };
 
 export default meta;
